60b5d50b20a630c5c10ae37c7903855693f01251
Vaishali Pazare <155642246+vaishaliPazare7@users.noreply.github.com>
2025-04-22 08:31:36 +0530

    About.tsx

--- src/About.tsx
+++ src/About.tsx
@@ -89,7 +89,7 @@ const About: React.FC = () => {
                   d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                 />
               </svg>
-              <span className="font-medium">Email:</span> support@ourapp.com
+              <span className="font-medium">Email:</span> vaishalipazare651@gmail.com
             </p>
             <p className="flex items-center">
               <svg
@@ -106,7 +106,7 @@ const About: React.FC = () => {
                   d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                 />
               </svg>
-              <span className="font-medium">Phone:</span> (123) 456-7890
+              <span className="font-medium">Phone:</span> (7447)622879
             </p>
             <p className="flex items-center">
               <svg
@@ -129,8 +129,7 @@ const About: React.FC = () => {
                   d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
                 />
               </svg>
-              <span className="font-medium">Address:</span> 123 App Street, San
-              Francisco, CA 94107
+              <span className="font-medium">Address:</span> Pune, Maharshtra 
             </p>
           </div>
         </div>
